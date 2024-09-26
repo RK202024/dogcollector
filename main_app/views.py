21e1c915589757a862dc5a3fba02dfa18d06ed81
rk202025 <reedkrosby@hotmail.com>
2024-09-26 13:33:39 -0400

    DogCollector

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+  return HttpResponse('<h1>Hello Dog!</h1>')
+
+def about(request):
+  return HttpResponse('<h1>About the Dog Collector</h1>')
 
 # Create your views here.
