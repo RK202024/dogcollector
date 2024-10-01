bbc7499bd13860cb44b5e148ad7341d06d8904bb
rk202025 <reedkrosby@hotmail.com>
2024-10-01 08:48:48 -0400

    Dogcollector typo

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+class Dog:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Stevie', 'hound', 'Young and restless.', 3),
+  Dog('Genghis', 'frenchie', 'Badly behaved but very polite.', 0),
+  Dog('Pippa', 'mix', 'Speaks with her tail.', 4),
+  Dog('Bowie', 'scottie', 'Always the the older brother.', 6)
+]
+
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello Dog!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
-  return HttpResponse('<h1>About the Dog Collector</h1>')
+  return render(request, 'about.html')
 
-# Create your views here.
+def dog_index(request):
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
